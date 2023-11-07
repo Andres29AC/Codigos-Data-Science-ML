2d8b41a015e4ff2843a3d9d0772d19f98ba6583e
Andres Amaro <lionAS_45@hotmail.com>
2023-11-07 01:41:34 -0500

    Codigos de testing

---
+++ pruebas/codigo1.py
@@ -0,0 +1,21 @@
+#Testing example
+#Librerias de test unitario
+import unittest
+
+#Probando:
+def suma(a,b):
+    return a+b
+
+#Clase de testeo
+class Test(unittest.TestCase):
+    #Metodo de testeo
+    def test_suma(self):
+        self.assertEqual(suma(2,3),5)
+        self.assertEqual(suma(2,-3),-1)
+        self.assertEqual(suma(2,0),2)
+        self.assertEqual(suma(0,0),0)
+        self.assertEqual(suma(-2,-3),-5)
+
+#Ejecucion de testeo
+if __name__ == '__main__':
+    unittest.main()
